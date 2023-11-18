90a40ef1069fe25e74b474169d9ae7c8e9294907
demetre <duta.diasamidze@mail.ru>
2023-11-18 19:55:07 +0400

    Dias01-Shualeduri

---
+++ index.js
@@ -0,0 +1,37 @@
+
+let age = 20; 
+
+if (age >= 18) {
+    console.log(">18");
+} else {
+    console.log("<18");
+}
+
+let weekDay = 4; 
+
+switch (weekDay) {
+    case 1:
+        console.log("ორშაბათი");
+        break;
+    case 2:
+        console.log("სამშაბათი");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Your week day is not correct");
+        break;
+}
